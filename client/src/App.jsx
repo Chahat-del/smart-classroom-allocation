62ced4718ad5c780b04e0f2588d6b38a6657c630
Chahat Khandelwal <chahatkhandelwal2005@gmail.com>
2026-04-03 16:53:44 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import TeacherDashboard from "./pages/TeacherDashboard";
+import TeacherDashboard from "./pages/Teacherdashboard";
 import StudentDashboard from "./pages/StudentDashboard";
 import StudentSchedule from './pages/StudentSchedule';
 import StudentBookingRequest from './pages/StudentBookingRequest';
-
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Teacher page - no Navbar */}
         <Route path="/teacher" element={<TeacherDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} />
-          <Route path="/schedule" element={<StudentSchedule />} />
-          <Route path="/booking-request" element={<StudentBookingRequest />} />
-        <Route path="*" element={<Navigate to="/teacher" replace />} />
+        
+        {/* Student pages - with Navbar */}
+        <Route path="/*" element={
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/dashboard" element={<StudentDashboard />} />
+              <Route path="/schedule" element={<StudentSchedule />} />
+              <Route path="/booking-request" element={<StudentBookingRequest />} />
+              <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            </Routes>
+          </>
+        } />
       </Routes>
     </BrowserRouter>
   );
